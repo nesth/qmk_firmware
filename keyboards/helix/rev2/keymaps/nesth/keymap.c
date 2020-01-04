e45a98a723d9b12cde21c170fc5b430233c3fecb
Luc Lebel <luc.lebel@rubikloud.com>
2020-01-04 02:38:13 -0500

    default mode icon to tux

--- keyboards/helix/rev2/keymaps/nesth/keymap.c
+++ keyboards/helix/rev2/keymaps/nesth/keymap.c
@@ -64,8 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
       FUNC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
       ADJUST,  KC_LCTL, KC_LALT, _______, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE, KC_RGUI,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
@@ -411,7 +411,7 @@ static void render_layer_status(struct CharacterMatrix *matrix) {
 void render_status(struct CharacterMatrix *matrix) {
 
   // Render to mode icon
-  static const char os_logo[][2][3] PROGMEM  ={{{0x95,0x96,0},{0xb5,0xb6,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
+  static const char os_logo[][2][3] PROGMEM  ={{{0x99,0x9a,0},{0xb9,0xba,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
   if(keymap_config.swap_lalt_lgui==false){
     matrix_write_P(matrix, os_logo[0][0]);
     matrix_write_P(matrix, PSTR("\n"));
