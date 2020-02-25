35fe1c452ba12544ab10bbe7beca3bf0d58ed939
Luc Lebel <luc.lebel@rubikloud.com>
2020-02-25 10:27:36 -0500

    helix like crkbd

--- keyboards/helix/rev2/keymaps/nesth/keymap.c
+++ keyboards/helix/rev2/keymaps/nesth/keymap.c
@@ -31,6 +31,10 @@ enum layer_number {
     _ADJUST
 };
 
+// Custom keycodes for layer keys
+// Dual function escape with left command
+#define KC_LGESC LGUI_T(KC_ESC)
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -38,7 +42,8 @@ enum custom_keycodes {
   FUNC,
   ADJUST,
   BACKLIT,
-  RGBRST
+  RGBRST,
+  KC_RACL // right alt / colon
 };
 
 enum macro_keycodes {
@@ -67,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
       FUNC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-      KC_LCTL, KC_LCTL, KC_LALT, ADJUST,  KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE, KC_RGUI,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+      KC_LCTL, KC_LCTL, KC_LALT, ADJUST,  KC_LGESC,LOWER,   KC_SPC , KC_ENT,  RAISE, KC_RACL,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
@@ -85,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-      KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  _______, _______, KC_HOME, KC_END,  KC_RSFT, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY \
+      KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+      KC_LCTL, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,                KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_NO,   KC_NO, \
+      KC_LSFT, KC_NO,   KC_BTN2, KC_WH_D, KC_WH_U, KC_BTN1,KC_LPRN,KC_RPRN,KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   KC_NO, \
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_LGESC,LOWER,  KC_SPC, KC_ENT, RAISE,   KC_RACL, KC_NO,   KC_NO,   KC_NO,   KC_NO \
       ),
 
   /* Raise
@@ -106,11 +111,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-      KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY \
+      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+      KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+      KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV, \
+      KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD, \
+      _______, _______, _______, _______, KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RALT, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   /* Function
@@ -149,10 +154,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] =  LAYOUT( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-      _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL, \
-      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  _______, _______,  _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD \
+      RESET,   RGBRST,  _______, _______, _______, _______,                   _______, KC_MUTE, _______, _______, _______, KC_DEL, \
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______,                   KC_PAUSE, KC__VOLUP, _______, _______,  _______, _______, \
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, RGB_RMOD, \
+      _______, _______, _______, _______, KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  RAISE, KC_RALT, _______, _______, _______, _______ \
       )
 };
 
@@ -192,6 +197,7 @@ void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint16_t my_colon_timer;
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
@@ -275,6 +281,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       #endif
       return false;
       break;
+    case KC_RACL:
+        if (record->event.pressed) {
+          my_colon_timer = timer_read();
+          register_code(KC_RALT);
+        } else {
+          unregister_code(KC_RALT);
+          if (timer_elapsed(my_colon_timer) < TAPPING_TERM) {
+            SEND_STRING(":"); // Change the character(s) to be sent on tap here
+          }
+        }
+        return false;
     case RGBRST:
       #ifdef RGBLIGHT_ENABLE
         if (record->event.pressed) {
